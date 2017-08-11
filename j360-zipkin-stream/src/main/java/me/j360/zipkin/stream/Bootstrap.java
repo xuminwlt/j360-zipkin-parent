2ffbea1446668dde025aca4b106869d078496583
min_xu <xumin_wlt@163.com>
2017-08-11 17:35:40 +0800

    add env

--- j360-zipkin-stream/src/main/java/me/j360/zipkin/stream/Bootstrap.java
+++ j360-zipkin-stream/src/main/java/me/j360/zipkin/stream/Bootstrap.java
@@ -1,10 +1,17 @@
 package me.j360.zipkin.stream;
 
 
+import okhttp3.OkHttpClient;
 import zipkin.collector.CollectorComponent;
 import zipkin.collector.InMemoryCollectorMetrics;
 import zipkin.collector.kafka.KafkaCollector;
-import zipkin.storage.elasticsearch.ElasticsearchStorage;
+import zipkin.storage.StorageComponent;
+import zipkin.storage.elasticsearch.http.ElasticsearchHttpStorage;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Package: me.j360.trace.example.consumer1
@@ -13,14 +20,48 @@ import zipkin.storage.elasticsearch.ElasticsearchStorage;
  * 说明：原生启动方式
  */
 public class Bootstrap {
-
-    private static String zkHost = "172.16.10.125";
-    private static String kafkaHost = zkHost;
+    public final static String testKafkaUrl = "123.59.27.205:9092";
+    private static String kafkaServers = "qa-service:9092";
+    private static String zkServers = "123.59.27.210:2181";
+    private static Map<String, String> overrides = new HashMap<>();
+    private static String zipkinName = "zipkin";
 
     public static void main(String args[]){
+        overrides.put("enable.auto.commit", "true");
+        overrides.put("auto.commit.interval.ms", "1000");
+        overrides.put("session.timeout.ms", "30000");
+        overrides.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        overrides.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        StorageComponent storageComponent = toBuilder(client).build();
+
         InMemoryCollectorMetrics metrics = new InMemoryCollectorMetrics();
-        final CollectorComponent component = KafkaCollector.builder().zookeeper(zkHost+":2181").metrics(metrics).storage(ElasticsearchStorage.builder().build()).streams(1).build();
+        final CollectorComponent component = KafkaCollector.builder()
+                .groupId("j360-zipkin-stream")
+                .topic(zipkinName)
+                .zookeeper(zkServers)
+                //.bootstrapServers(kafkaServers)
+                .overrides(overrides)
+                .metrics(metrics)
+                .storage(storageComponent)
+                .streams(1)
+                .build();
         component.start();
+    }
+
+    public static ElasticsearchHttpStorage.Builder toBuilder(OkHttpClient client) {
+        List<String> hosts = Collections.singletonList("http://123.59.27.210:9200");
+
+        ElasticsearchHttpStorage.Builder builder = ElasticsearchHttpStorage.builder(client);
+        if (hosts != null) builder.hosts(hosts);
+        return builder
+                .index(zipkinName)
+                .dateSeparator('-')
+                //.pipeline(pipeline)
+                .maxRequests(100)
+                .indexShards(5)
+                .indexReplicas(1);
     }
 
 }
